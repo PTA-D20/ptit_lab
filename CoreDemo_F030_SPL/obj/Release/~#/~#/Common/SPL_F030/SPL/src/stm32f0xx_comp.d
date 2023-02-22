ba4f4ce3142722d3fb8de9a5b051e5c8ea0998de
pham the anh_d20 <125725025+PTA-D20@users.noreply.github.com>
2023-02-22 11:44:41 +0700

    Add files via upload

---
+++ CoreDemo_F030_SPL/obj/Release/~#/~#/Common/SPL_F030/SPL/src/stm32f0xx_comp.d
@@ -0,0 +1,31 @@
+obj\release\~\#\~\#\common\spl_f030\spl\src\stm32f0xx_comp.o: \
+ ..\..\Common\SPL_F030\SPL\src\stm32f0xx_comp.c \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_comp.h \
+ ..\..\Common\SPL_F030\inc/stm32f0xx.h \
+ ..\..\Common\SPL_F030\cmsis/core_cm0.h \
+ ..\..\Common\SPL_F030\cmsis/core_cmInstr.h \
+ ..\..\Common\SPL_F030\cmsis/core_cmFunc.h \
+ ..\..\Common\SPL_F030\inc/system_stm32f0xx.h \
+ ..\..\Common\SPL_F030\inc/stm32f0xx_conf.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_adc.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_can.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_cec.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_crc.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_crs.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_dac.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_dbgmcu.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_dma.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_exti.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_flash.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_gpio.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_syscfg.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_i2c.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_iwdg.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_pwr.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_rcc.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_rtc.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_spi.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_tim.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_usart.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_wwdg.h \
+ ..\..\Common\SPL_F030\SPL\inc/stm32f0xx_misc.h
---
+++ CoreDemo_F030_SPL/src/main.c
@@ -0,0 +1,72 @@
+
+#include "Core.h"
+#include <stdio.h>
+
+/**********************************************************************/
+#define LED_PIN         4
+#define LED_PORT        GPIOA
+#define LED_PIN_SPEED   GPIO_Speed_2MHz
+
+#define LED_LOW()       LED_PORT->BRR  = _BV(LED_PIN)
+#define LED_HIGH()      LED_PORT->BSRR = _BV(LED_PIN)
+#define LED_TOGGLE()    BITMASK_TGL(LED_PORT->ODR, _BV(LED_PIN))
+
+/**********************************************************************/
+void GPIO_begin() {
+  // enable GPIO clock
+  BITMASK_SET(RCC->AHBENR, RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB);
+
+  F030_GPIO_pinMode_output(LED_PORT, LED_PIN, GPIO_Mode_OUT, GPIO_OType_PP, LED_PIN_SPEED);
+}
+
+
+/**********************************************************************/
+// main
+/**********************************************************************/
+int main(void) {
+  /// Setup
+  Core_HSIConfig();
+//Core_EraseOptionBytes();
+
+  Core_begin();
+  GPIO_begin();
+
+  USART1_begin(BAUD_1M);
+  /// Cấu hình và khởi động USART1, chi tiết cấu hình xem trong file LibraryCfg.c
+  Core_PrintInfo();
+  /// in thông tin về cấu hình flash ra serial monitor
+
+  /// Loop
+  uint32_t delay_target = 0;
+  while (1) {
+    printf("%12u\t", micros());
+    /// in thời gian ở micro giây ra màn hình
+    printf("%9u\t", millis());
+    /// in thời gian ở milli giây ra màn hình
+    printf("\n");
+
+    LED_TOGGLE();
+    delay_until_ms(delay_target += 100);
+    LED_TOGGLE();
+    /// đảo trạng thái đèn led sau mỗi 100ms.
+
+    if (USART1_available() > 0) {
+      while (USART1_available() > 0) {
+        printf("%c", USART1_read());
+     }
+     /// in kí tự được gửi đến từ serial monitor
+      printf("\n");
+      LED_TOGGLE();
+    }
+    /// cả đoạn chương trình trên nhằm giúp in ra màn hình monitor những gì được nhập vào ở serial monitor
+
+    delay_ms(1000);
+    delay_until_ms(delay_target += 900);
+    /// chờ 1900ms
+
+    LED_TOGGLE();
+    delay(50);
+  }
+}
+
+/**********************************************************************/
